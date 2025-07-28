c144fc2d949c36ec4907dfb4027a29a7ff3afaf1
Vincent Privat <vincent.privat@airbus.com>
2025-07-28 13:42:38 +0200

    Add typing information

--- rs_dpr_service/main.py
+++ rs_dpr_service/main.py
@@ -43,7 +43,7 @@ from rs_dpr_service.openapi_validation import (
     validate_request,
     validate_response,
 )
-from rs_dpr_service.processors import S1L0Processor, S3L0Processor
+from rs_dpr_service.processors import GeneralProcessor, S1L0Processor, S3L0Processor
 from rs_dpr_service.utils import init_opentelemetry
 from rs_dpr_service.utils.logging import Logging
 from rs_dpr_service.utils.utils import env_bool
@@ -53,7 +53,7 @@ from rs_dpr_service.utils.utils import env_bool
 from . import jobs_table  # pylint: disable=unused-import
 
 # Register all the processors
-processors = {
+processors: dict[str, type[GeneralProcessor]] = {
     "S1L0_processor": S1L0Processor,
     "S3L0_processor": S3L0Processor,
     "Conversion_Processor": ConversionProcessor,
